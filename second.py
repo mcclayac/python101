2fd8ed61ed5bfbc623cf10f4cf5c218bd94860ce
mcclayac <anthony.mcclay@me.com>
2015-10-27 00:22:54 -0400

    Initial commit
    This is the first checkin

---
+++ second.py
@@ -0,0 +1,11 @@
+__author__ = 'anthonymcclay'
+__project__ = 'python101'
+__date__ = '10/27/15'
+__revision__ = '$'
+__revision_date__ = '$'
+
+
+
+
+print("This is my first Python code for githib")
+
